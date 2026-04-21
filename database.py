b4b2cfd9bbdc401ea12a638c1dbf23c3cbcbd154
PoohSorawit <118596123+poohlikung@users.noreply.github.com>
2026-04-22 00:42:39 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Request
 from typing import Union
 from pydantic import BaseModel
 # SQL Alchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+
 
 # create sql engine
 sqlite_file_name = "database.db"
@@ -18,5 +21,26 @@ SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
 
 
+# ORM Class
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Integer)
+
+# สร้างฐานข้อมูล
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
+
+# base
+class Item(BaseModel):
+    title:str
+    description:str
+    price:float
+
+class ItemCreate(Item):
+    pass
